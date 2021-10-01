9b4053d25f8b87e0cb8aa75e667e72b6f99cb8b4
morikawa.kenta.mystery@mail.com <morikawa.kenta.mystery@gmail.com>
2021-10-01 22:35:37 +0900

    settingsのdebugをtrueに変えた

--- sns_project/settings.py
+++ sns_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'kskz7jru#32da#@cl^d5q3_m8o$ykvp+49y336v66lme(rin9f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ["snsforbasketball.herokuapp.com"]
@@ -132,6 +132,6 @@ try:
 except ImportError:
     pass
 
-if not DEBUG:
+if DEBUG:
     import django_heroku
     django_heroku.settings(locals())
